a1c1998355edc9c859a8c5e4bedc8ff7d2939199
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-19 15:16:09 +0200

    Add new wallpaper

--- scripts/computer_ui.gd
+++ scripts/computer_ui.gd
@@ -2,15 +2,6 @@ extends Control
 
 @onready var label: Label = $SignalIcon/Label
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func opened() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	print("Opened computer")
