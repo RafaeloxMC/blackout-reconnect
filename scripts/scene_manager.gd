8155bbb6a9abbbcb224989a589669762ea09caa3
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-11 18:42:20 +0100

    Add scene manager

---
+++ scripts/scene_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var scenes: Dictionary[String, PackedScene]
+@onready var transition: ColorRect = $Transition
+
+@export var scene_history: Array[PackedScene] = []
+
+func call_packed(scene: PackedScene) -> void:
+	transition.fwd()
+	await get_tree().create_timer(transition.duration).timeout
+	get_tree().change_scene_to_packed(scene)
+	scene_history.append(scene)
+	transition.rev()
+
+func call_scene(scene: String) -> void:
+	if scenes.get(scene):
+		call_packed(scenes.get(scene))
+
+func back():
+	call_packed(scene_history[scene_history.size() - 2])
