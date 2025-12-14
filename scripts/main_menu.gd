95d92c99c2c98ec47aa36484281b759e86bba853
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-14 08:54:25 +0100

    Add VSync button to main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,22 @@
 extends Control
 
+@onready var v_sync: Button = $UIElements/VSync
+
+func _ready() -> void:
+	v_sync.text = "VSync: On" if DisplayServer.window_get_vsync_mode() == DisplayServer.VSyncMode.VSYNC_ENABLED else "VSync: Off"
+
 func _on_play_pressed() -> void:
 	SceneManager.call_scene("game")
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_v_sync_pressed() -> void:
+	if DisplayServer.window_get_vsync_mode() == DisplayServer.VSyncMode.VSYNC_ENABLED:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		v_sync.text = "VSync: Off"
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+		v_sync.text = "VSync: On"
